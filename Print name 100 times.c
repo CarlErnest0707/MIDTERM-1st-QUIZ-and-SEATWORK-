c38d6d78b250787f45ef5e15bd3d95147f3b32f1
CarlErnest0707 <2025-1-01107@lpu.edu.ph>
2025-09-30 08:37:46 +0800

    Add files via upload

---
+++ Print name 100 times.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    printf("This program prints my name 100 times in 'for' method. ");
+    int i;
+    for (i = 0; i < 100; i++) {
+        printf("Carl Ernest Lleo S. Laririt\n");
+    }
+    return 0;
+}
